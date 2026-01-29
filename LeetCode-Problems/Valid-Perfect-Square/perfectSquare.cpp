b0a72e407fdfd0a8f3ee25ccb21a572f0d8dc3ac
kashish sharma <princysharma9990563069@gmail.com>
2026-01-29 21:32:06 +0530

    feat :  optimized binary search for valid perfect square.
    Implemented O(log N) binary search with guard clauses for n==0 and n==1 to ensure safe n/2 search space reduction. Added long long mitigation for integer overflow handling.

---
+++ LeetCode-Problems/Valid-Perfect-Square/perfectSquare.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool perfectSqrt(int n){
+  // Handle negative numbers and small edge cases
+    if(n==0 || n==1 ) return true;
+    if(n<0) return false;
+
+    int st=0 , end = n/2;
+    
+    while(st <= end){
+        long long mid = st + (end - st)/ 2 ; 
+        long long square = mid*mid; // prevent overflow
+        if( square == n)  return true;
+        else if(square < n){
+            st = mid +1;
+        }else{
+            end = mid-1;
+        }
+    } 
+    return false;
+}
+int main(){
+    int x;
+    cout<<" Enter integer value = ";
+    cin>>x;
+    cout<< perfectS
+      qrt(x);
+    return 0;
+}
